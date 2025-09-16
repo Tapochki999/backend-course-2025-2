928e73790f801653a83d90fcb390737fbf9e0c64
Volodymyr Husiak bc2025-2 <zenorchalt@gmail.com>
2025-09-16 08:49:51 +0300

    Updated file urls.txt with new URL

---
+++ Main.js
@@ -0,0 +1,1 @@
+course://web.backend?name=%22%D0%92%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%20%D0%93%D1%83%D1%81%D1%8F%D0%BA%22&group=%22%D0%A4%D0%95%D0%86-24-2%D1%81%22&subgroup=%22%D0%9F%D1%96%D0%B4%D0%B3%D1%80%D1%83%D0%BF%D0%B0%201%22#HusiakVolodymyr%0D%0A
