b4b5d3f2791be4026b275209545c0221d169520c
Volodymyr Husiak bc2025-2 <zenorchalt@gmail.com>
2025-09-16 09:57:28 +0300

    Modified Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,14 @@
-course://web.backend?name=%22%D0%92%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%20%D0%93%D1%83%D1%81%D1%8F%D0%BA%22&group=%22%D0%A4%D0%95%D0%86-24-2%D1%81%22&subgroup=%22%D0%9F%D1%96%D0%B4%D0%B3%D1%80%D1%83%D0%BF%D0%B0%201%22#HusiakVolodymyr%0D%0A
+const currency = "usd";        
+const date_from = "20220707";    
+const date_to = "20220730";      
+
+const url = new URL("https://bank.gov.ua/NBU_Exchange/exchange_site");
+
+url.searchParams.set("start", date_from);
+url.searchParams.set("end", date_to);
+url.searchParams.set("valcode", currency);
+url.searchParams.set("sort", "exchangedate");
+url.searchParams.set("order", "desc");
+url.searchParams.set("json", "true");
+
+console.log(url.toString());
